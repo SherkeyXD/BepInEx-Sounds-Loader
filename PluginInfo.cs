8c82f87cc04e4a3bd2063ab7d4f9d8e1bf9c0e65
SherkeyXD <57581480+SherkeyXD@users.noreply.github.com>
2024-12-21 15:10:43 +0800

    publish: v1.0.1

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,5 +2,5 @@ public static class PluginInfo
 {
     public const string PLUGIN_GUID = "com.sherkeyxd.misidesoundsloader";
     public const string PLUGIN_NAME = "MiSideSoundsLoader";
-    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_VERSION = "1.0.1";
 }
